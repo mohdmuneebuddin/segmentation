0f77d5eabd971c7aa34fa0debf2a2227cee537ba
Mohammed Muneebuddin <muneebuddin136@gmail.com>
2026-04-23 18:08:04 +0300

    Update train.py

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ def main():
             error = torch.mean((recon - images)**2, dim = [1,2,3])
             train_errors.extend(error.cpu().numpy())
 
-    threshold = np.percentile(train_errors, 95)
+    threshold = np.percentile(train_errors, 99)
     print(f"Threshold: {threshold}")
     model.eval()
     all_labels = []
@@ -80,8 +80,37 @@ def main():
             images, labels = images.to(device), labels.to(device)
             recon = model(images)
             error = torch.mean((images - recon) ** 2, dim = [1,2,3])
+            
             all_scores.extend(error.cpu().numpy())
             all_labels.extend(labels.cpu().numpy())
+                        error_map = (images - recon)**2
+            heatmap = error_map.mean(dim=1)   # shape: (B, H, W)
+            heatmap = heatmap[0].cpu().numpy()
+            heatmap = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min() + 1e-8)
+            img = images[0].cpu().permute(1, 2, 0).numpy()
+            import matplotlib.pyplot as plt
+
+            plt.figure(figsize=(12,4))
+            
+            plt.subplot(1,3,1)
+            plt.title("Original")
+            plt.imshow(img)
+            plt.axis("off")
+            
+            plt.subplot(1,3,2)
+            plt.title("Reconstruction")
+            recon_img = recon[0].cpu().permute(1,2,0).numpy()
+            plt.imshow(recon_img)
+            plt.axis("off")
+            
+            plt.subplot(1,3,3)
+            plt.title("Anomaly Heatmap")
+            plt.imshow(img)
+            plt.imshow(heatmap, cmap='jet', alpha=0.5)  # overlay
+            plt.axis("off")
+            
+            plt.show()
+            break
     preds = [1 if s > threshold else 0 for s in all_scores]
 
     acc = accuracy_score(all_labels, preds)
