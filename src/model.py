902e19aa7b6002c3745d2cf9b0e0c181a0ce78f7
Mohammed Muneebuddin <muneebuddin136@gmail.com>
2026-04-24 20:40:59 +0300

    Add U-Net autoencoder model implementation
    Implement U-Net autoencoder architecture with double convolution blocks for image processing tasks.

---
+++ src/model.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torchvision.transforms as TF
+import matplotlib.pyplot as plt
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+    def forward(self, x):
+        return self.conv(x)
+
+class UNetAutoencoder(nn.Module):
+    def __init__(self, in_channels, out_channels, features=[64, 128, 256, 512]):
+        super(Unet, self).__init__()
+        self.downs = nn.ModuleList()
+        self.up = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        self.bottleneck =DoubleConv(features[-1], 2*features[-1])
+
+        for feature in reversed(features):
+            self.up.append(nn.ConvTranspose2d(2*feature, feature, kernel_size=2, stride=2))
+            self.up.append(DoubleConv(2*feature, feature))
+
+        self.final = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.up), 2):
+            x = self.up[idx](x)
+            skip_connection = skip_connections[idx // 2]
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.up[idx + 1](concat_skip)
+
+        return self.final(x)
+
+
+
+
